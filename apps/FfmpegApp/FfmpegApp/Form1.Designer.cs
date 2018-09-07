de72884e2570efbbbe982b585c0e8a38a6238560
RAHIMPC\Abdur Rahim <rahim.prsf@gmail.com>
2018-09-07 18:01:53 +0600

    Implement multiple files convertion with modified UI

--- apps/FfmpegApp/FfmpegApp/Form1.Designer.cs
+++ apps/FfmpegApp/FfmpegApp/Form1.Designer.cs
@@ -28,124 +28,174 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.convertButton = new System.Windows.Forms.Button();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.openFileButton = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.folderOpenButton = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.SuspendLayout();
-            // 
-            // convertButton
-            // 
-            this.convertButton.Location = new System.Drawing.Point(98, 172);
-            this.convertButton.Name = "convertButton";
-            this.convertButton.Size = new System.Drawing.Size(75, 23);
-            this.convertButton.TabIndex = 0;
-            this.convertButton.Text = "Convert";
-            this.convertButton.UseVisualStyleBackColor = true;
-            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
-            // 
-            // openFileDialog1
-            // 
-            this.openFileDialog1.FileName = "openFileDialog1";
-            this.openFileDialog1.Filter = "Video Files|*.flv";
-            // 
-            // openFileButton
-            // 
-            this.openFileButton.Location = new System.Drawing.Point(641, 42);
-            this.openFileButton.Name = "openFileButton";
-            this.openFileButton.Size = new System.Drawing.Size(28, 23);
-            this.openFileButton.TabIndex = 1;
-            this.openFileButton.Text = "...";
-            this.openFileButton.UseVisualStyleBackColor = true;
-            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(42, 47);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Input file";
-            // 
-            // textBox1
-            // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.textBox1.Location = new System.Drawing.Point(98, 43);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(537, 20);
-            this.textBox1.TabIndex = 3;
-            // 
-            // textBox2
-            // 
-            this.textBox2.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.textBox2.Location = new System.Drawing.Point(98, 87);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(537, 20);
-            this.textBox2.TabIndex = 4;
-            // 
-            // folderOpenButton
-            // 
-            this.folderOpenButton.Location = new System.Drawing.Point(641, 86);
-            this.folderOpenButton.Name = "folderOpenButton";
-            this.folderOpenButton.Size = new System.Drawing.Size(28, 23);
-            this.folderOpenButton.TabIndex = 5;
-            this.folderOpenButton.Text = "...";
-            this.folderOpenButton.UseVisualStyleBackColor = true;
-            this.folderOpenButton.Click += new System.EventHandler(this.folderOpenButton_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(18, 91);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(71, 13);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Output Folder";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(5, 135);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(84, 13);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Output Filename";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(98, 131);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(487, 20);
-            this.textBox3.TabIndex = 7;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.folderOpenButton);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.openFileButton);
-            this.Controls.Add(this.convertButton);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.convertButton = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.addFilesButton = new System.Windows.Forms.Button();
+			this.folderOpenButton = new System.Windows.Forms.Button();
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.label1 = new System.Windows.Forms.Label();
+			this.fileListBox = new System.Windows.Forms.ListBox();
+			this.numberOfFilesLabel = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.ouputDirLabel = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.statusLabel = new System.Windows.Forms.Label();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.removeAllButton = new System.Windows.Forms.Button();
+			this.removeSelectctedButton = new System.Windows.Forms.Button();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// convertButton
+			// 
+			this.convertButton.Location = new System.Drawing.Point(452, 12);
+			this.convertButton.Name = "convertButton";
+			this.convertButton.Size = new System.Drawing.Size(104, 23);
+			this.convertButton.TabIndex = 0;
+			this.convertButton.Text = "Convert";
+			this.convertButton.UseVisualStyleBackColor = true;
+			this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			this.openFileDialog1.Filter = "Video Files|*.flv";
+			this.openFileDialog1.Multiselect = true;
+			// 
+			// addFilesButton
+			// 
+			this.addFilesButton.Location = new System.Drawing.Point(12, 12);
+			this.addFilesButton.Name = "addFilesButton";
+			this.addFilesButton.Size = new System.Drawing.Size(104, 23);
+			this.addFilesButton.TabIndex = 1;
+			this.addFilesButton.Text = "Add files";
+			this.addFilesButton.UseVisualStyleBackColor = true;
+			this.addFilesButton.Click += new System.EventHandler(this.openFileButton_Click);
+			// 
+			// folderOpenButton
+			// 
+			this.folderOpenButton.Location = new System.Drawing.Point(122, 12);
+			this.folderOpenButton.Name = "folderOpenButton";
+			this.folderOpenButton.Size = new System.Drawing.Size(104, 23);
+			this.folderOpenButton.TabIndex = 5;
+			this.folderOpenButton.Text = "Chenge output directory";
+			this.folderOpenButton.UseVisualStyleBackColor = true;
+			this.folderOpenButton.Click += new System.EventHandler(this.folderOpenButton_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(3, 8);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(77, 13);
+			this.label1.TabIndex = 7;
+			this.label1.Text = "Number of files";
+			// 
+			// fileListBox
+			// 
+			this.fileListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.fileListBox.FormattingEnabled = true;
+			this.fileListBox.Location = new System.Drawing.Point(12, 124);
+			this.fileListBox.Name = "fileListBox";
+			this.fileListBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+			this.fileListBox.Size = new System.Drawing.Size(544, 316);
+			this.fileListBox.TabIndex = 8;
+			// 
+			// numberOfFilesLabel
+			// 
+			this.numberOfFilesLabel.AutoSize = true;
+			this.numberOfFilesLabel.Location = new System.Drawing.Point(92, 8);
+			this.numberOfFilesLabel.Name = "numberOfFilesLabel";
+			this.numberOfFilesLabel.Size = new System.Drawing.Size(13, 13);
+			this.numberOfFilesLabel.TabIndex = 9;
+			this.numberOfFilesLabel.Text = "0";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(3, 31);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(79, 13);
+			this.label2.TabIndex = 10;
+			this.label2.Text = "Ouput directory";
+			// 
+			// ouputDirLabel
+			// 
+			this.ouputDirLabel.AutoSize = true;
+			this.ouputDirLabel.Location = new System.Drawing.Point(92, 31);
+			this.ouputDirLabel.Name = "ouputDirLabel";
+			this.ouputDirLabel.Size = new System.Drawing.Size(0, 13);
+			this.ouputDirLabel.TabIndex = 11;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(3, 54);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(37, 13);
+			this.label3.TabIndex = 12;
+			this.label3.Text = "Status";
+			// 
+			// statusLabel
+			// 
+			this.statusLabel.AutoSize = true;
+			this.statusLabel.Location = new System.Drawing.Point(92, 54);
+			this.statusLabel.Name = "statusLabel";
+			this.statusLabel.Size = new System.Drawing.Size(0, 13);
+			this.statusLabel.TabIndex = 13;
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.statusLabel);
+			this.panel1.Controls.Add(this.numberOfFilesLabel);
+			this.panel1.Controls.Add(this.label3);
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.ouputDirLabel);
+			this.panel1.Location = new System.Drawing.Point(12, 41);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(544, 77);
+			this.panel1.TabIndex = 14;
+			// 
+			// removeAllButton
+			// 
+			this.removeAllButton.Location = new System.Drawing.Point(232, 12);
+			this.removeAllButton.Name = "removeAllButton";
+			this.removeAllButton.Size = new System.Drawing.Size(104, 23);
+			this.removeAllButton.TabIndex = 15;
+			this.removeAllButton.Text = "Remove all";
+			this.removeAllButton.UseVisualStyleBackColor = true;
+			this.removeAllButton.Click += new System.EventHandler(this.removeAllButton_Click);
+			// 
+			// removeSelectctedButton
+			// 
+			this.removeSelectctedButton.Location = new System.Drawing.Point(342, 12);
+			this.removeSelectctedButton.Name = "removeSelectctedButton";
+			this.removeSelectctedButton.Size = new System.Drawing.Size(104, 23);
+			this.removeSelectctedButton.TabIndex = 16;
+			this.removeSelectctedButton.Text = "Remove selected";
+			this.removeSelectctedButton.UseVisualStyleBackColor = true;
+			this.removeSelectctedButton.Click += new System.EventHandler(this.removeSelectctedButton_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(569, 450);
+			this.Controls.Add(this.removeSelectctedButton);
+			this.Controls.Add(this.removeAllButton);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.fileListBox);
+			this.Controls.Add(this.folderOpenButton);
+			this.Controls.Add(this.addFilesButton);
+			this.Controls.Add(this.convertButton);
+			this.Name = "Form1";
+			this.Text = "Convert to mp4";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
 
         }
 
@@ -153,15 +203,19 @@
 
         private System.Windows.Forms.Button convertButton;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.Button openFileButton;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button addFilesButton;
         private System.Windows.Forms.Button folderOpenButton;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox3;
-    }
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ListBox fileListBox;
+		private System.Windows.Forms.Label numberOfFilesLabel;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label ouputDirLabel;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label statusLabel;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button removeAllButton;
+		private System.Windows.Forms.Button removeSelectctedButton;
+	}
 }
 
--- apps/FfmpegApp/FfmpegApp/Form1.cs
+++ apps/FfmpegApp/FfmpegApp/Form1.cs
@@ -1,67 +1,118 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FfmpegApp
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();            
-        }
+	public partial class Form1 : Form
+	{
+		private string _defaultOutputDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Videos");
 
-        private void convertButton_Click(object sender, EventArgs e)
-        {
-            string output =textBox2.Text+ $"\\{textBox3.Text}-{DateTime.Now.Ticks}.mp4";
-            LaunchCommandLineApp(textBox1.Text, output);
-            Process.Start("explorer.exe", "/select, \"" + output + "\"");
-        }
+		public Form1()
+		{
+			InitializeComponent();
+			openFileDialog1.Multiselect = true;
+			ouputDirLabel.Text = _defaultOutputDir;
+		}
 
-        private void LaunchCommandLineApp(string input, string outputFile)
-        {
-            ProcessStartInfo startInfo = new ProcessStartInfo();
-            startInfo.CreateNoWindow = false;
-            startInfo.UseShellExecute = false;
-            startInfo.FileName = "ffmpeg.exe";
-            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            startInfo.Arguments = $"-i \"{input}\" \"{outputFile}\"";
-            try
-            {
-                using (Process exeProcess = Process.Start(startInfo))
-                {
-                    exeProcess.WaitForExit();
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-        }
+		private void convertButton_Click(object sender, EventArgs e)
+		{
+			if (fileListBox.Items.Count > 0)
+			{
+				convertButton.Enabled = false;
+				addFilesButton.Enabled = false;
+				folderOpenButton.Enabled = false;
 
-        private void openFileButton_Click(object sender, EventArgs e)
-        {
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
-            {
-                textBox1.Text = openFileDialog1.FileName;
-            }
-        }
+				statusLabel.Text = "Converting...";
 
-        private void folderOpenButton_Click(object sender, EventArgs e)
-        {
-            folderBrowserDialog1.SelectedPath = Environment.CurrentDirectory;
-            if (folderBrowserDialog1.ShowDialog()==DialogResult.OK)
-            {
-                textBox2.Text = folderBrowserDialog1.SelectedPath;
-            }
-        }
-    }
+				int numberOfFiles = fileListBox.Items.Count;
+				int counter = 1;
+
+				foreach (string item in fileListBox.Items)
+				{
+					string output = _defaultOutputDir + $"\\{Path.GetFileNameWithoutExtension(item)}-{DateTime.Now.Ticks}.mp4";
+					LaunchCommandLineApp(item, output);
+					statusLabel.Text = counter++ + " File(s) converted";
+				}
+
+				Process.Start("explorer.exe", "\"" + _defaultOutputDir + "\"");
+
+				statusLabel.Text = "Completed.";
+
+				convertButton.Enabled = true;
+				addFilesButton.Enabled = true;
+				folderOpenButton.Enabled = true;
+			}
+			else
+			{
+				MessageBox.Show("No files are selected.");
+			}
+		}
+
+		private void LaunchCommandLineApp(string input, string outputFile)
+		{
+			ProcessStartInfo startInfo = new ProcessStartInfo();
+			startInfo.CreateNoWindow = false;
+			startInfo.UseShellExecute = false;
+			startInfo.FileName = "ffmpeg.exe";
+			startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+			startInfo.Arguments = $"-i \"{input}\" \"{outputFile}\"";
+			startInfo.CreateNoWindow = true;
+			try
+			{
+				using (Process exeProcess = Process.Start(startInfo))
+				{
+					exeProcess.WaitForExit();
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+			}
+		}
+
+		private void openFileButton_Click(object sender, EventArgs e)
+		{
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				foreach (string file in openFileDialog1.FileNames)
+				{
+					if (fileListBox.Items.Contains(file)) continue;
+					fileListBox.Items.Add(file);
+				}
+			}
+
+			numberOfFilesLabel.Text = fileListBox.Items.Count.ToString();
+
+			if (fileListBox.Items.Count > 0)
+			{
+				statusLabel.Text = "Pending...";
+			}
+
+		}
+
+		private void folderOpenButton_Click(object sender, EventArgs e)
+		{
+			folderBrowserDialog1.SelectedPath = Environment.CurrentDirectory;
+			if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+			{
+				ouputDirLabel.Text = folderBrowserDialog1.SelectedPath;
+				_defaultOutputDir = folderBrowserDialog1.SelectedPath;
+			}
+		}
+
+		private void removeAllButton_Click(object sender, EventArgs e)
+		{
+			fileListBox.Items.Clear();
+		}
+
+		private void removeSelectctedButton_Click(object sender, EventArgs e)
+		{
+			while (fileListBox.SelectedItems.Count > 0)
+			{
+				fileListBox.Items.Remove(fileListBox.SelectedItems[0]);
+			}
+		}
+	}
 }
